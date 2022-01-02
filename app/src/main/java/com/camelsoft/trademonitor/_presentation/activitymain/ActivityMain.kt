1d362ba42453829dd5ab55244a1395f1f08bfbb0
Maximus <accauntmmg@gmail.com>
2022-01-03 01:14:04 +0400

    baenki

--- app/src/main/java/com/camelsoft/trademonitor/_presentation/activitymain/ActivityMain.kt
+++ app/src/main/java/com/camelsoft/trademonitor/_presentation/activitymain/ActivityMain.kt
@@ -2,6 +2,9 @@ package com.camelsoft.trademonitor._presentation.activitymain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import com.camelsoft.trademonitor.R
 import com.camelsoft.trademonitor.databinding.ActivityMainBinding
 
 class ActivityMain : AppCompatActivity() {
@@ -11,5 +14,20 @@ class ActivityMain : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.apply {
+            navView.setNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.navMenuMonitor -> { Toast.makeText(this@ActivityMain, "Hello", Toast.LENGTH_SHORT).show() }
+                    R.id.navMenuSettings -> { }
+                    R.id.navMenuExit -> { finish() }
+                    else -> {}
+                }
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+
+        }
+
     }
 }
