cd3616d7677a20a9101b29a56caa33dd8cffe509
Maximus <accauntmmg@gmail.com>
2022-01-03 20:10:10 +0400

    permissions

--- app/src/main/java/com/camelsoft/trademonitor/_presentation/activitymain/ActivityMain.kt
+++ app/src/main/java/com/camelsoft/trademonitor/_presentation/activitymain/ActivityMain.kt
@@ -2,9 +2,9 @@ package com.camelsoft.trademonitor._presentation.activitymain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.core.view.GravityCompat
-import androidx.fragment.app.Fragment
 import com.camelsoft.trademonitor.R
 import com.camelsoft.trademonitor.databinding.ActivityMainBinding
 
@@ -16,37 +16,35 @@ class ActivityMain : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Верхняя панель
         setSupportActionBar(binding.activityMainContent.mainToolbar)
+        //supportActionBar?.title = resources.getString(R.string.title_menu)
 
-        binding.apply {
-            mainNavView.setNavigationItemSelectedListener {
-                when (it.itemId) {
-                    R.id.navMenuHome -> { startFragment(FragmentMainHome.newInstance()) }
-                    R.id.navMenuDir -> { }
-                    R.id.navMenuSettings -> { startFragment(FragmentMainSettings.newInstance()) }
-                    R.id.navMenuExit -> { finish() }
-                    else -> {}
-                }
-                mainDrawerLayout.closeDrawer(GravityCompat.START)
+        // Нажатия Navigation-списка
+        binding.mainNavView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navMenuDir -> { }
+                R.id.navMenuSettings -> {  }
+                R.id.navMenuExit -> { finish() }
+                else -> {}
+            }
+            binding.mainDrawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
+    // Нажатия кнопок верхнего меню
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                binding.mainDrawerLayout.openDrawer(GravityCompat.START)
                 true
             }
-
+            else -> { super.onOptionsItemSelected(item) }
         }
-
     }
 
-    override fun onResume() {
-        super.onResume()
-        startFragment(FragmentMainHome.newInstance())
-    }
-
-    private fun startFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.mainFrameLayout, fragment)
-            .commit()
-    }
-
+    // Выход
     private var back_pressed: Long = 0
     override fun onBackPressed() {
         if (back_pressed + 2000 > System.currentTimeMillis())
